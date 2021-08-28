fb48a803c27fae44a2651b1a3c431adad421c0c3
Mesquitinha <gustavogava123@hotmail.com>
2021-08-27 21:15:30 -0300

    Button to change between Fahrenheit and Celsius, and some improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react"
 
-import axios from "axios";
+import { api } from "../services/api"
 
-import "../assets/styles/Home.css";
+import "../assets/styles/Home.css"
 
 export function Home() {
-  const [city, setCity] = useState("City");
-  const [country, setCountry] = useState("Country");
-  const [weather, setWeather] = useState("Sunny");
-  const [temp, setTemp] = useState("15");
-  const [input, setInput] = useState("");
-  const [unit, setUnit] = useState("metric");
+  const [city, setCity] = useState("City")
+  const [country, setCountry] = useState("Country")
+  const [weather, setWeather] = useState("Sunny")
+  const [temp, setTemp] = useState("")
+  const [input, setInput] = useState("")
+  const [inputBackup, setInputBackup] = useState("London")
+  const [unit, setUnit] = useState("metric")
 
-  var data = new Date();
-  var months = [
+  const data = new Date()
+  const months = [
     "January",
     "February",
     "March",
@@ -26,58 +27,84 @@ export function Home() {
     "October",
     "November",
     "December",
-  ];
+  ]
 
-  async function getWeather(city, unidade) {
+  useEffect(() => {
+    getWeather("London", "metric")
+  }, [])
+
+  useEffect(() => {
+    getWeather(inputBackup, unit)
+  }, [unit])
+
+  async function getWeather(city, unit) {
     try {
-      let weather = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather`,
-        {
-          params: {
-            q: city,
-            appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
-            units: unidade,
-          },
-        }
-      );
-      let tempStr = JSON.stringify(weather.data.main.temp);
-      setTemp(tempStr.replace(".", ","));
-      setWeather(weather.data.weather[0].main);
-      setCity(weather.data.name);
-      setCountry(weather.data.sys.country);
+      const weather = await api.get("weather/", {
+        params: {
+          q: city,
+          appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
+          units: unit,
+        },
+      })
+      let tempStr = JSON.stringify(weather.data.main.temp)
+      setTemp(tempStr.replace(".", ","))
+      setWeather(weather.data.weather[0].main)
+      setCity(weather.data.name)
+      setCountry(weather.data.sys.country)
     } catch {
-      alert("Check your input!");
+      alert("Check your input!")
     }
   }
 
-  async function handleSearchClick() {
-    await getWeather(input, unit);
-    setInput("");
+  async function handleSearchClick(event) {
+    event.preventDefault()
+    await getWeather(input, unit)
+    setInputBackup(input)
+    setInput("")
   }
 
   return (
     <div className="main-container">
-      <header>
+      <form onSubmit={handleSearchClick}>
         <input
           type="text"
           placeholder="City, State or Country"
           onChange={(e) => setInput(e.target.value)}
           value={input}
         />
-        <button onClick={handleSearchClick}>Search</button>
-      </header>
+        <button type="submit">Search</button>
+      </form>
+
       <div className="information-container">
-        <h2 className="location" onClick={getWeather}>
+        <h2 className="location">
           {city}, {country}
         </h2>
         <p>{`${
           months[data.getMonth()]
         }  ${data.getDate()}, ${data.getFullYear()}`}</p>
         <div className="container-degrees">
-          <span>{temp}°C</span>
+          <span>
+            {temp}
+            {unit === "imperial" ? "°F" : "°C"}
+          </span>
         </div>
         <h2>{weather}</h2>
       </div>
+
+      <div className="change-unit-container">
+        <button
+          onClick={() => setUnit("imperial")}
+          className={unit === "imperial" ? "active" : {}}
+        >
+          Fahrenheit
+        </button>
+        <button
+          onClick={() => setUnit("metric")}
+          className={unit === "metric" ? "active" : {}}
+        >
+          Celsius
+        </button>
+      </div>
     </div>
-  );
+  )
 }
